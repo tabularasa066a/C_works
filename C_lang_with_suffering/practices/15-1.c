e082c2c60ac6b71260fb06a865d2fc18797c3e26
tabularasa066a <tabularasa066a@gmail.com>
2021-04-11 15:41:05 +0900

    15-1

--- C_lang_with_suffering/practices/15-1.c
+++ C_lang_with_suffering/practices/15-1.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 // １−１：アドレス, ポインタ
 // ２−１：valueのアドレスを参照してその内容を書き換えているため
-// ４−１：
+// ４−１：ある変数へのショートカットのこと
 
 // ３−１：
-void maxmin(int array[], int max, int min);
+void maxmin(int array[], int *max, int *min);
 
 int main(void) {
+  int i = 0, array[10], max, min;
+
+  do {
+    printf("%d番目の数：", i+1);
+    scanf("%d", &array[i]);
+    i++;
+  } while (array[i-1] != -1);
+
+  maxmin(array, &max, &min);
+
+  printf("最大値%d：最小値%d\n", max, min);
 
   return 0;
 }
 
-void maxmin(int array[], int max, int min){
+void maxmin(int array[], int *max, int *min){
+  int i = 0;
+
+  *max = 0;
+  *min = 100;
+
+  while (array[i] != -1) {
+    if (array[i] > *max) *max = array[i];
+    if (array[i] < *min) *min = array[i];
+    i++;
+  }
 
 }
